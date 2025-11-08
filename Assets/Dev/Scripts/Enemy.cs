673adce0e0a4089b1deabd218b8c12af196f1565
Sein819 <seinb819@gmail.com>
2025-11-08 17:29:40 +0900

    적 변수 선언&피해, 사망 구현

--- Assets/Dev/Scripts/Enemy.cs
+++ Assets/Dev/Scripts/Enemy.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    MaterialPropertyBlock mpb;
+
+    public float maxHp;
+    public float hp;
+    public float attackPower;
+    public float attackSpeed;
+    [HideInInspector]
+    public bool dead;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        mpb = new MaterialPropertyBlock();
+
+        maxHp=100;
+        hp=maxHp;
+        attackPower=100;
+        attackSpeed=100;
+        dead=false;
+    }
+
+    void Update(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //피해 입기
+    public IEnumerator HitColor(){
+        sr.GetPropertyBlock(mpb);
+        mpb.SetFloat("_IsDamaged", 1f);
+        sr.SetPropertyBlock(mpb);
+        yield return new WaitForSeconds(0.3f);
+        sr.GetPropertyBlock(mpb);
+        mpb.SetFloat("_IsDamaged", 0f);
+        sr.SetPropertyBlock(mpb);
+    }
+
+    //사망
+    public void Die(){
+        Destroy(gameObject);
     }
 }
