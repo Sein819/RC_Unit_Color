86bd8046bbd2d913c694328df2105234e1705a4e
Sein819 <seinb819@gmail.com>
2025-11-08 18:36:36 +0900

    적 소환 구현

--- Assets/Dev/Scripts/GameManager.cs
+++ Assets/Dev/Scripts/GameManager.cs
@@ -10,8 +10,10 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject player;
     public Rigidbody2D pRigid;
-    
+    public GameObject enemy;
+
     float gameTime;
+    float enemyAmount;
 
     void Awake(){
         if(instance==null){
@@ -21,14 +23,29 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         gameTime=0;
+        enemyAmount=0;
     }
 
     void Start(){
-        
+        SpawnEnemy(10);
     }
 
     void Update(){
         gameTime+=Time.deltaTime; 
+    }
+
+    void SpawnEnemy(int amount){
+        for(int i=0;i<amount;i++){
+            Instantiate(enemy,new Vector2(Random.Range(-5.5f,5.5f),Random.Range(-5.5f,5.5f)),transform.rotation);
+            enemyAmount=amount;
+        }
+    }
+
+    public void EnmeyDie(){
+        enemyAmount--;
+        if(enemyAmount<=0){
+            SpawnEnemy(10);
+        }
     }
 
     public IEnumerator GameOver(){
